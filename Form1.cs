a63e49fe1fb13ba575350c72edc6f0498a0e7426
KaanSalman1 <100555439+KaanSalman1@users.noreply.github.com>
2022-03-07 11:38:51 +0300

    Queue Koleksiyon Uygulaması Kodlarım

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QueueKoleksiyonUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Queue kuyruk = new Queue();
+        private int sira = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sira++;
+            kuyruk.Enqueue(sira);
+            Listele();
+        }
+
+        private void Listele()
+        {
+            listBox1.Items.Clear();
+            foreach (int sira in kuyruk)
+            {
+                listBox1.Items.Add(sira);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            Listele();
+        }
+    }
+}
